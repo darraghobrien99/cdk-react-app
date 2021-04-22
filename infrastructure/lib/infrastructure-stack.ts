c019978bcdd31de7949df8c0478b13b52528978f
Darragh O'Brien <darraghmobrien@hotmail.com>
2021-04-22 21:16:42 +0100

    Second Commit

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -1,10 +1,15 @@
 import * as cdk from '@aws-cdk/core';
 import * as s3 from '@aws-cdk/aws-s3';
 import * as iam from '@aws-cdk/aws-iam';
+import * as dynamodb from '@aws-cdk/aws-dynamodb';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as apigateway from '@aws-cdk/aws-apigateway';
 import { AttributeType, Table } from '@aws-cdk/aws-dynamodb';
 import { Construct, RemovalPolicy, Stack, StackProps } from '@aws-cdk/core';
 
+
 export class InfrastructureStack extends cdk.Stack {
+  public readonly handler: lambda.Function;
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
@@ -22,12 +27,31 @@ export class InfrastructureStack extends cdk.Stack {
 })
 bucket.addToResourcePolicy(bucketPolicy); // 4 
 
-const tableName = 'movies';
-
-    new Table(this, 'moviesTable', {
+//Create Movies Table in DynamoDB
+const tableName = 'Movies';
+   const moviesTable =  new Table(this, 'moviesTable', {
       tableName,
-      partitionKey: { name: 'id', type: AttributeType.STRING },
+      partitionKey: { name: 'title', type: AttributeType.STRING },
       removalPolicy: RemovalPolicy.DESTROY,
     });
+
+const moviesLambda = new lambda.Function(this, 'MoviesHandler', {
+      code: lambda.Code.fromAsset('../client/src/lambda'),
+      
+      handler: 'client/src/lambda/moviesHandler.handler',
+      runtime: lambda.Runtime.NODEJS_14_X,
+      memorySize: 256,
+      environment: {
+        TABLE_NAME:moviesTable.tableName,
+      },
+    });
+
+    moviesTable.grantReadWriteData(moviesLambda);
+
+    new apigateway.LambdaRestApi(this, 'MoviesEndpoint', {
+      handler: moviesLambda,
+    });
+
+  
 }
 }
